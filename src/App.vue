8b049ff90b071a0dd41df9fc0d8e42689ecd7e92
Тумановский Владислав Сергеевич <449384@kali.by>
2022-07-05 17:20:13 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <Header/>
+    <RouterView />
+</template>
+
+<script>
+import Header from '@/components/core/Header.vue'
+export default {
+    components: {
+        Header,
+    }
+}
+</script>
+<style>
+@import "@/assets/base.css";
+
+</style>
